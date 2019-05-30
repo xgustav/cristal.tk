2f785fafdf00bb3dedbb0300ff01d5c913750dc9
Niv Sardi <xaiki@evilgiggle.com>
2019-05-30 15:25:04 -0300

    integrate hero css and images from template
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ src/components/Hero/images/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export {default as cloud_full_2 } from './cloud_full_2.png'
+export {default as cloud_full_3 } from './cloud_full_3.png'
+export {default as cloud_full } from './cloud_full.png'
+export {default as cloud_left_full } from './cloud_left_full.png'
+export {default as cloud_left } from './cloud_left.png'
+export {default as cloud } from './cloud.png'
