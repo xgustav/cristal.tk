d31cd2122ce5d0b3fdc9e7bbfe7c78a84757a837
tuti <ptutino@gmail.com>
2019-06-06 22:48:51 -0300

    Supporters s images resized. Supporters s images classname changed. Removed hover menu from header items.

--- src/sections/Collage/index.tsx
+++ src/sections/Collage/index.tsx
@@ -22,7 +22,7 @@ export const Collage: React.FunctionComponent<ICollage> = ({ images, title, subt
                 <div className="flex justify-center flex-wrap items-center">
                     {images.map((image, key) => (
                         <div key={key} className="sm:w-1/2 sm:p-6 p-8 text-center">
-                            <Image className="h-12" src={image.src} alt={image.alt} size="sm" />
+                            <Image className="h-40" src={image.src} alt={image.alt} size="sm" />
                         </div>
                     ))}
                 </div>
